8ed542a806ad2da3a2b6b6cf6f1231dc44208d25
MrFreuden <mrfreuden@gmail.com>
2024-04-02 12:31:40 +0200

    34

--- Test4/Form1.Designer.cs
+++ Test4/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(347, 177);
+            label1.Location = new Point(348, 217);
             label1.Name = "label1";
             label1.Size = new Size(56, 20);
             label1.TabIndex = 0;
